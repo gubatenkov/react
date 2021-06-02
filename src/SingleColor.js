bb5bbbd10b3cc5ae142bd2072759f5554c07b970
V <s9379992g@gmail.com>
2021-06-02 18:00:01 +0300

    init commit

---
+++ src/SingleColor.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index, hexCode }) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hex = rgbToHex(...rgb);
+  const hexValue = `#${hexCode}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  const handleCopy = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexValue);
+  };
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={handleCopy}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
